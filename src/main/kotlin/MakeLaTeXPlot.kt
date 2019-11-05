ec7f9b3ae5acfeab5579e83c51bead82b571b0b2
lsafina <safinalrr@gmail.com>
2019-11-05 13:29:47 +0100

    cleaning

--- src/main/kotlin/MakeLaTeXPlot.kt
+++ src/main/kotlin/MakeLaTeXPlot.kt
@@ -4,38 +4,64 @@ import java.time.LocalTime
 import java.util.concurrent.TimeUnit
 import kotlin.math.pow
 import kotlin.math.sqrt
+import java.sql.DriverManager.println
+import java.util.ArrayList
+
 
 fun main() {
-    cleanFolders()
-    generatePlot("comms-only", "numberOfActions", "time(msec)", listOf("Random", "InteractionsFirst", "ConditionsFirst"))
-    runLatex()
-    mergePDF()
+    cleanTempFolders() //remove all previously generated files
+
+    //generate a data source for a plot with different test parameters and different extraction strategies
+    generatePlot(TEST_TYPES.COMMS_ONLY, X.NUMBER_OF_ACTIONS, Y.TIME, listOf(
+            STRATEGY.RANDOM,
+            STRATEGY.INTERACTION_FIRST,
+            STRATEGY.SHORTEST_FIRST,
+            STRATEGY.SHORTEST_FIRST), "risl")
+
+    generatePlot(TEST_TYPES.COMMS_ONLY, X.NUMBER_OF_ACTIONS, Y.NODES, listOf(
+            STRATEGY.RANDOM,
+            STRATEGY.INTERACTION_FIRST,
+            STRATEGY.SHORTEST_FIRST,
+            STRATEGY.SHORTEST_FIRST), "risl")
+
+    runLatex() //compile latex file to pdf
+    mergePDF() //combine all pdf to one TestResult.pdf
 }
 
-fun generatePlot(testType: String, x: String, y: String, strategyList: List<String>) {
-    println("[${LocalTime.now()}] Start generating laTex multi-lines plot files...")
-    val latexGenDir = File(LATEX_GEN_DIR)
+/***
+ * generate a data source plot for each or required strategy with data of 2 required columns
+ */
+fun generatePlot(testType: TEST_TYPES, x: X, y: Y, strategyList: List<STRATEGY>, strategiesSuffix: String = "") {
+    val testType = testType.value
+    val x = x.value
+    val y = y.value
 
-    val yy = if (y == "time(msec)") "time" else y
-    val fileTitle = "stats-by-$x-to-$yy"
-    val genLatexFile = File("$latexGenDir/$fileTitle.tex")
+    println("[${LocalTime.now()}] Start generating laTex multi-lines plot files...")
+
+    val latexGenDir = File(LATEX_GEN_DIR)
+    val yFixed = if (y == "time(msec)") "time" else y //for some mysterious reasons brackets breaks everything
+
+    val fileTitle = "stats-by-$x-to-$yFixed-$strategiesSuffix.tex"
+    val genLatexFile = File("$latexGenDir/$fileTitle")
 
     genLatexFile.bufferedWriter().use { out ->
 
         for (strategy in strategyList) {
+            val strategy = strategy.value
             val testFileName = "$PREFIX-$strategy-$testType"
             val testFile = File("$TESTS_DIR/$testFileName")
 
             if (testFile.exists()) {
 
-                val pairXtoY = extractColumnsData(x, y, testFile)
+                val pairXtoY = extractColumnsData(x, y, testFile) //get data for a plot
                 out.appendln(
-                    "\\pgfplotstableread{\n" +
-                            "$x $y-mean $y-err"
+                        "\\pgfplotstableread{\n" +
+                                "$x $y-mean $y-err"
                 )
+
                 for ((valX, valsY) in pairXtoY.toSortedMap()) {
                     out.appendln(
-                        "$valX ${mean(valsY)} ${stderr(valsY)}"
+                            "$valX ${mean(valsY)} ${stderr(valsY)}"
                     )
                 }
                 out.appendln("}\\${testType.filter { it.isLetter() }}$strategy")
@@ -43,103 +69,39 @@ fun generatePlot(testType: String, x: String, y: String, strategyList: List<Stri
         }
 
         out.append(
-            "\\begin{figure}[!h]\n" +
-                    "\\centering\n" +
-                    "\\begin{tikzpicture}[yscale=1.5, xscale=1.5]\n" +
-                    "\\begin{axis}[xlabel=$x,ylabel=$y, legend style={at={(0.5,1.6)},anchor=north}]\n"
+                "\\begin{figure}[!h]\n" +
+                        "\\centering\n" +
+                        "\\begin{tikzpicture}[yscale=1.5, xscale=1.5]\n" +
+                        "\\begin{axis}[xlabel=$x,ylabel=$y, legend style={at={(0.5,1.6)},anchor=north}]\n"
         )
 
         for (strategy in strategyList) {
+            val strategy = strategy.value
             out.appendln(
 
-                "\\addplot+[smooth]\n" +
-                        "table[\n" +
-                        "    x=$x,\n" +
-                        "    y=$y-mean]\n" +
-                        "{\\${testType.filter { it.isLetter() }}$strategy};\n" +
-                        "\\addlegendentry{$strategy}"
+                    "\\addplot+[smooth]\n" +
+                            "table[\n" +
+                            "    x=$x,\n" +
+                            "    y=$y-mean]\n" +
+                            "{\\${testType.filter { it.isLetter() }}$strategy};\n" +
+                            "\\addlegendentry{$strategy}"
             )
         }
 
         out.append(
-            "\\end{axis}\n" +
-                    "\\end{tikzpicture}\n" +
-                    "\\caption[]{$testType}\n" +
-                    "\\label{fig:test}\n" +
-                    "\\end{figure}\n\n"
+                "\\end{axis}\n" +
+                        "\\end{tikzpicture}\n" +
+                        "\\caption[]{$testType}\n" +
+                        "\\label{fig:test}\n" +
+                        "\\end{figure}\n\n"
         )
     }
 
 }
 
-fun generatePlots(listOfTests: List<String> = TEST_TYPES, listOfX: List<String> = X, listOfY: List<String> = Y, listOfStrategies: List<String> = STRATEGY) {
-    println("[${LocalTime.now()}] Start generating laTex multi-lines plot files...")
-    val latexGenDir = File(LATEX_GEN_DIR)
-
-    var counter = 1
-
-    for (x in listOfX) {
-        for (y in listOfY) {
-
-            val yy = if (y == "time(msec)") "time" else y
-            val fileTitle = "stats-by-$x-to-$yy"
-            val genLatexFile = File("$latexGenDir/$fileTitle.tex")
-
-            genLatexFile.bufferedWriter().use { out ->
-                for (testType in listOfTests) {
-                    for (strategy in listOfStrategies) {
-                        val testFileName = "$PREFIX-$strategy-$testType"
-                        val testFile = File("$TESTS_DIR/$testFileName")
-
-                        if (testFile.exists()) {
-
-                            val pairXtoY = extractColumnsData(x, y, testFile)
-                            out.appendln(
-                                "\\pgfplotstableread{\n" +
-                                        "$x $y-.mean $y-err"
-                            )
-                            for ((valX, valsY) in pairXtoY.toSortedMap()) {
-                                out.appendln(
-                                    "$valX ${mean(valsY)} ${stderr(valsY)}"
-                                )
-                            }
-                            out.appendln("}\\${testType.filter { it.isLetter() }}$strategy")
-                        }
-                    }
-
-                    out.append(
-                        "\\begin{figure}[!h]\n" +
-                                "\\centering\n" +
-                                "\\begin{tikzpicture}[yscale=1.5, xscale=1.5]\n" +
-                                "\\begin{axis}[xlabel=$x,ylabel=$y, legend style={at={(0.5,1.6)},anchor=north}]\n"
-                    )
-
-                    for (strategy in listOfStrategies) {
-                        out.appendln(
-
-                            "\\addplot+[smooth]\n" +
-                                    "table[\n" +
-                                    "    x=$x,\n" +
-                                    "    y=$y-mean]\n" +
-                                    "{\\${testType.filter { it.isLetter() }}$strategy};\n" +
-                                    "\\addlegendentry{$strategy}"
-                        )
-                    }
-
-                    out.append(
-                        "\\end{axis}\n" +
-                                "\\end{tikzpicture}\n" +
-                                "\\caption[]{$testType}\n" +
-                                "\\label{fig:test${counter++}}\n" +
-                                "\\end{figure}\n\n"
-                    )
-                }
-            }
-        }
-    }
-
-}
-
+/**
+ * retrieves data of 2 required columns from the file with statistics
+ */
 private fun extractColumnsData(x: String, y: String, testFile: File): HashMap<Double, ArrayList<Double>> {
     val lines = testFile.readLines()
     val split = lines.first().split("\t")
@@ -160,44 +122,42 @@ private fun extractColumnsData(x: String, y: String, testFile: File): HashMap<Do
             list.add(valY)
             pairXtoY[valX] = list
         }
-        /*}
-        catch (e: Exception) {
-            println(indexOfX)
-            println(lineSplit.size)
-            println(testFile)
-        }*/
-
-
     }
     return pairXtoY
 }
 
+/**
+ * for each generated plot template, put it for a processing by lualatex to LATEX_GEN_PLOT, remove after processing
+ */
 fun runLatex() {
     println("[${LocalTime.now()}] Start processing laTex files...")
     val latexGenDir = File(LATEX_GEN_DIR)
 
-    for (file in latexGenDir.list()) {
+    for (file in latexGenDir.list()) { //TODO replace path strings on const vals, check if anything was generated
         println("[${LocalTime.now()}] Generate pdf for $file...")
         val processBuilder = ProcessBuilder()
-        val waitFor = processBuilder.command(
-            "bash",
-            "-c",
-            "cd $LATEX_GEN_DIR/; cp $file ../latex/gen/file.tex; cd ../latex/; lualatex -interaction nonstopmode main.tex; mv main.pdf ../genPDF/$file.pdf"
+        val waitFor = processBuilder.command(//copy each generated plot template for a folder where it will be compiled by lualatex
+                "bash",
+                "-c",
+                "cd $LATEX_GEN_DIR/; cp $file ../processLatex/plot/file.tex; cd ../processLatex/; lualatex -interaction nonstopmode main.tex; mv main.pdf ../genPDF/$file.pdf"
         ).start().waitFor(30, TimeUnit.SECONDS)
         if (!waitFor) println("[${LocalTime.now()}] FAIL. No pdf for $file was generated")
 
-        processBuilder.command(
-            "bash",
-            "-c",
-            "cd $LATEX_PROCESS_DIR/; rm file.tex"
-        )
+        processBuilder.command( //remove used plot
+                "bash",
+                "-c",
+                "cd $LATEX_GEN_PLOT/; rm file.tex"
+        ).start()
     }
 }
 
-fun mergePDF() {
+/**
+ * merge pdf results together
+ */
+fun mergePDF() {    //TODO: if there is one file, do not merge but rename
     println("[${LocalTime.now()}] Start merging pdfs...")
+    val dir = File(PDF_GEN_DIR)
     val ut = PDFMergerUtility()
-    val dir = File(PDF_DIR)
 
     for (file in dir.list())
         ut.addSource("$dir/$file")
@@ -205,8 +165,11 @@ fun mergePDF() {
     ut.mergeDocuments()
 }
 
-fun cleanFolders() {
-    for (dirName in listOfDirs) {
+/**
+ * remove previously generated files
+ */
+fun cleanTempFolders() {
+    for (dirName in listOfTempDirs) {
         val dir = File(dirName)
         dir.deleteRecursively()
         dir.mkdir()
@@ -227,120 +190,4 @@ fun stderr(list: ArrayList<Double>): Double {
     val res = stddev(list) / sqrt(list.size.toDouble())
     return if (res.isNaN()) 0.0 else res
 }
-//endregion
-
-//region backlog
-/*fun forallParam(testDir: String) {
-    println("[${LocalTime.now()}] Start generating latex boilerplate...")
-    val dir = File(testDir)
-    //if (!dir.exists() || !dir.isDirectory) dir.mkdir()
-
-    val res = File(.LATEX_GEN_DIR)
-    if (!res.exists() || !res.isDirectory) res.mkdir()
-    var counter = 0
-
-    for (file in dir.list()) {
-        if (.getTEST_TYPES.any { elem -> file.endsWith(elem) }) {
-            for (.getX in COLUMN_TYPES_IMPORTANT) {
-                println("[${LocalTime.now()}] Generating file for $file-by-$kotlin.getX")
-                val test = File("$kotlin.LATEX_GEN_DIR/$file-by-$kotlin.getX.tex")
-                test.bufferedWriter().use { out ->
-                    for (y in COLUMN_TYPES_IMPORTANT) {
-                        if (.getX != y) {
-                            out.appendln(
-                                "\\begin{figure}\n" +
-                                        "\\begin{tikzpicture}\n" +
-                                        "\\begin{axis}[xlabel=$kotlin.getX,ylabel=$y]\n" +
-                                        "\\addplot [scatter,only marks] \n" +
-                                        "table[.getX=$kotlin.getX,y=$y]\n" +
-                                        "{statistics/$file};\n" +
-                                        "\\end{axis}\n" +
-                                        "\\end{tikzpicture}\n" +
-                                        "\\caption[]{$kotlin.getX to $y ($file)}\n" +
-                                        "\\label{fig:test${counter++}}\n" +
-                                        "\\end{figure}\n"
-                            )
-                        }
-                    }
-                }
-            }
-
-        }
-    }
-}*/
-
-/*
-fun parseFileNamesByStrategy(){
-    val dir = File(RESULT_DIR)
-    val destDir = File("final")
-
-
-    val tempDir = File("tempDir")
-    tempDir.mkdir()
-
-    COLUMN_TYPES_IMPORTANT.forEach { column ->
-        println(column)
-        val ut = PDFMergerUtility()
-        for (file in dir.list()){
-            if (file.contains(column)){
-                ut.addSource("$dir/$file")
-            }
-        }
-        ut.destinationFileName = "$destDir/by-$column-TestResults.pdf"
-        ut.mergeDocuments()
-    }
-}
-
-fun generateLaTexByParams(listOfTests: ArrayList<String>, listOfX: ArrayList<String>, listOfY: ArrayList<String>, listOfStrategies: ArrayList<String>) {
-    println("[${LocalTime.now()}] Start generating laTex files...")
-    val testsDir = File(.TESTS_DIR)
-    val latexGenDir = File(.LATEX_GEN_DIR)
-
-    var counter = 1
-
-    for (x in listOfX) {
-        for (y in listOfY) {
-
-            val yy = if (y == "time(msec)") "time" else y
-            val fileTitle = "stats-by-$x-to-$yy"
-            val genLatexFile = File("$latexGenDir/$fileTitle.tex")
-
-            genLatexFile.bufferedWriter().use { out ->
-                out.appendln("\\section{$x to $y}")
-
-                for (testType in listOfTests) {
-                    out.appendln(
-                        "\\clearpage\n" +
-                                "\\subsection{$testType}\n" +
-                                "\\clearpage"
-                    )
-                    for (file in testsDir.list().sorted()) {
-                        if (file.endsWith("$testType.txt")) {
-                            val strategy = file.split("-")[1]
-
-                            out.appendln(
-                                "\\begin{figure}\n" +
-                                        "\\centering\n" +
-                                        "\\begin{tikzpicture}\n" +
-                                        "\\begin{axis}[xlabel=$x,ylabel=$y]\n" +
-                                        "\\addplot [scatter,only marks] \n" +
-                                        "table[x=$x,y=$y]\n" +
-                                        "{statistics/$file};\n" +
-                                        "\\end{axis}\n" +
-                                        "\\end{tikzpicture}\n" +
-                                        "\\caption[]{$x to $y ($strategy)}\n" +
-                                        "\\label{fig:test${counter++}}\n" +
-                                        "\\end{figure}\n\n"
-                            )
-
-                        }
-
-                    }
-                }
-
-            }
-        }
-    }
-}
-*/
 //endregion
--- src/main/kotlin/PlotParameters.kt
+++ src/main/kotlin/PlotParameters.kt
@@ -1,80 +1,48 @@
 const val PREFIX = "stats"
-const val ROOT_DIR = "latexPlotData"
-const val TESTS_DIR = "$ROOT_DIR/latex/statistics"
-const val LATEX_GEN_DIR = "$ROOT_DIR/genLatex"
-const val LATEX_PROCESS_DIR = "$ROOT_DIR/latex/gen"
-const val PDF_DIR = "$ROOT_DIR/genPDF"
 
-val listOfDirs = listOf(
-    PDF_DIR,
-    LATEX_PROCESS_DIR,
+const val ROOT_DIR = "latexPlotData"
+const val TESTS_DIR = "$ROOT_DIR/statistics"
+const val LATEX_GEN_DIR = "$ROOT_DIR/genLatex"
+const val LATEX_PROCESS_DIR = "$ROOT_DIR/processLatex/"
+const val LATEX_GEN_PLOT = "$LATEX_PROCESS_DIR/plot"
+const val PDF_GEN_DIR = "$ROOT_DIR/genPDF"
+
+val listOfTempDirs = listOf(
+    PDF_GEN_DIR,
+    LATEX_GEN_PLOT,
     LATEX_GEN_DIR
 )
 
-val TEST_TYPES = listOf(
-    "all",
-    "comms-only",
-    "increasing-ifs-no-recursion",
-    "increasing-ifs-procedures",
-    "increasing-processes",
-    "test1",
-    "test2",
-    "test3-0",
-    "test3-5",
-    "test4"
-)
+enum class TEST_TYPES(val value: String) {
+    ALL("all"),
+    COMMS_ONLY("comms-only"),
+    INCREASING_IFS_NO_RECURSION("increasing-ifs-no-recursion"),
+    INCREASING_IFS_PROCEDURES("increasing-ifs-procedures"),
+    INCREASING_PROCESSES("increasing-processes")
+}
 
-val COLUMN_TYPES = listOf(
-    "numberOfActions",
-    "numberOfProcesses",
-    "numberOfProcedures",
-    "numberOfConditionals",
-    "minLengthOfProcesses",
-    "maxLengthOfProcesses",
-    "avgLengthOfProcesses",
-    "minNumberOfProceduresInProcesses",
-    "maxNumberOfProceduresInProcesses",
-    "avgNumberOfProceduresInProcesses",
-    "minNumberOfConditionalsInProcesses",
-    "maxNumberOfConditionalsInProcesses",
-    "avgNumberOfConditionalsInProcesses",
-    "numberOfProcessesWithConditionals",
-    "minProcedureLengthInProcesses",
-    "maxProcedureLengthInProcesses",
-    "avgProcedureLengthInProcesses",
-    "time(msec)",
-    "nodes",
-    "badLoops",
-    "mainLength",
-    "numOfProcedures",
-    "minProcedureLength",
-    "maxProcedureLength",
-    "avgProcedureLength"
-)
+enum class STRATEGY(val value: String) {
+    RANDOM("Random"),
+    INTERACTION_FIRST("InteractionsFirst"),
+    CONDITION_FIRST("ConditionsFirst"),
+    SHORTEST_FIRST("ShortestFirst"),
+    LONGEST_FIRST("LongestFirst"),
+    UNMARKED_FIRST("UnmarkedFirst"),
+    UNMARKED_THEN_CONDITION("UnmarkedThenCondition"),
+    UNMARKED_THEN_RANDOM("UnmarkedThenRandom"),
+    UNMARKED_THEN_SELECTIONS("UnmarkedThenSelections")
+}
 
+enum class X(val value: String) {
+    NUMBER_OF_ACTIONS("numberOfActions"),
+    NUMBER_OF_PROCESSES("numberOfProcesses"),
+    NUMBER_OF_PROCEDURES("numberOfProcedures"),
+    NUMBER_OF_CONDITIONALS("numberOfConditionals"),
+    AVG_NUMBER_OF_CONDITIONALS("avgNumberOfConditionalsInProcesses")
+}
 
-val STRATEGY = listOf(
-    "Random",
-    "InteractionsFirst",
-    "ConditionsFirst",
-    "ShortestFirst",
-    "LongestFirst",
-    "UnmarkedFirst",
-    "UnmarkedThenCondition",
-    "UnmarkedThenRandom",
-    "UnmarkedThenSelections"
-)
-
-val X = listOf(
-    "numberOfActions",
-    "numberOfProcesses",
-    "numberOfProcedures",
-    "numberOfConditionals",
-    "avgNumberOfConditionalsInProcesses"
-)
-
-val Y = listOf(
-    "time(msec)",
-    "nodes",
-    "badLoops"
-)
+enum class Y(val value: String) {
+    TIME("time(msec)"),
+    NODES("nodes"),
+    BAD_LOOPS("badLoops")
+}
